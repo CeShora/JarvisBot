b8c885cfcf452143105532b06c84ad6dae8d5bfa
neginkheirmand <neginkheirmand@gmail.com>
2021-09-15 01:13:23 +0430

    never mind im clean coding... kinda

---
+++ messages.py
@@ -0,0 +1,71 @@
+start_message = """Salam👋 khosh umadi🎉🥳🎉🥳🎉🥳🎉
+man jarvis😉 hastam, dastyar to tuye hafte haye avalet tuye 
+daneshgah va daneshkade, gharare behet komak konam yekam 
+rahat tar va sari tar ja biofT, soalat ro javab bedam va 
+moshkelatet ro hal konam."""
+
+parenthood_message = """khob, service pedar👨‍👦 madar👩‍👦 yabi. Inja gharare ye pedar/madar 
+peida konin🎉. Vali darim ziadi jelo mirim, ehtemalan alan 
+aslan nemidunin pedar/madar daneshjuE chi hast.
+Age be shomareye daneshjuEtun deghat karde bashin ye 
+hamchin chizie:
+4️⃣0️⃣0️⃣3️⃣1️⃣🔢🔢🔢
+ke 4️⃣0️⃣0️⃣ neshun dahandeye sal vurud va 3️⃣1️⃣ code reshte CE💻
+hast va 3 ragham akhar(🔢🔢🔢) faghat male shoma az bein 
+hamdoreE hatun hast.
+khob sal ghabl shoma ham vurudi hash ye hamchin 
+shomare daneshjuE dashtan. yeki az 99 ha shomareye 
+daneshjuEsh kheili shabih be male shomas va tanha farghesh 
+tuye un 3 ragham aval ke baraye una 9️⃣9️⃣ hast be jaye 4️⃣0️⃣0️⃣.
+Un fard pedar/madar daneshgahi shoma hast. Jalebe bedunin 
+ke in rasm faghat tuye daneshgahe AUT hast, ya laaghal injas 
+ke vaghean jedi gerefte mishe. Pedar/Madar khodetun ro age 
+mikhain beshnaCn mn behetun komak khahad kard. """
+
+tashakolat_message = """Aut ye serie tashakol👥 dare ke darvaghe ye guruh az bachehaye 
+khode daneshgah hastan ke (ba tavajoh be tashakoleshun) ye tedadi 
+az service haro erae midan be bachehaye dige.
+1)✨Shoraye senfi✨: Ehtemalan in tashakol ziba :)) ro ghabl az baghie 
+mishnaCn, in tashakol az hughugh amuzeshi(peigiri vahed📚🔖 ha...), 
+farhangi(📕puyesh, club haye film🎟, serial, kuhnavardi🌄 ...), 
+ertebat umumi(az esmesh malume 📸😉🎙), senfi(🏗👨🏻‍🔧), varzeshi(⚽️🏓🛹), mali(💵🧮) ... bacheha defa 
+mikone va tuye in zamine ha baraye bacheha refah tamin mikone. 
+Mesle vakiletune tuye in aspect ha. 
+moteshakel az:
+Aylar sedaE: 📧email...
+Bardia Ardakanian: 📧email...
+Rojina Kashefi: 📧email...
+Mahmud saneEyan: 📧email...
+Shayan Bali: 📧email...
+Kasra zareE: 📧email...
+Negin kheirmand: 📧email...
+
+va site khode shora:
+http://shora.ceit.aut.ac.ir/
+va 💬telegram💬:
+...
+-----------------inja social media haye shora---------------------
+
+2) 👨‍🏫anjoman elmi👩‍🏫:
+-inja hamina ro migim bara anjoman -
+...
+
+3) F.A.N.U.S(?)
+-inja hamina ro migim bara fanus(?) -
+..."""
+
+sites_message= """ 
+🌐site haye daneshgah🌐:
+1️⃣ ye courses darim ke tuye doran majazi platform asli baraye 
+upload tamarin o emtehan o ... bude:
+https://courses.aut.ac.ir/
+2⃣ ye lms darim ke platform asli amuzesh bude:
+https://lms.aut.ac.ir/
+3️⃣ ye portal amuzeshi hast ke bahash har term entekhab vahed mikonid 
+va nomarat porbar khodetun ro tush moshahede mikonid, hamchenin 
+ekhtar ha va ye serie az arzyabi ha mesl arzyabi asatid tush anjam mishe:
+https://portal.aut.ac.ir/
+va dar akhar
+4️⃣ ye samane samad ham darim ke felan be dardi nemikhore, 
+ghablan tush bacheha ghazaye self ro reserve mikardan:
+https://samad.aut.ac.ir/"""
