054664083ccd575568f297fe8e84e94d9c28ac05
neginkheirmand <neginkheirmand@gmail.com>
2021-09-15 04:18:26 +0430

    update code, its trash right now, you better not look

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 import logging
 import os
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
 
 from messages import * 
 
@@ -15,10 +24,45 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+GET_ID, GET_NAME, NAME_VALIDATION = range(3)
+
 def start(update, context):
-    """when student uses command /start """
     global start_message
     update.message.reply_text(start_message)
+    nextStep = "Hala baraye inke shuru konim, shomare daneshjuEt ro vared kon."
+    update.message.reply_text(nextStep)
+    return GET_ID
+
+def setID(update, context):
+    # now we have the id, we must get the user's name
+    studentId = update.message.text
+    user = update.message.from_user
+    logger.info("studentID of %s: %s", user.first_name, update.message.text)
+    getName = "Lotfan esme va familit ro vared kon"
+    update.message.reply_text(getName)
+    return GET_NAME
+    
+def nameSet(update, context):
+    name = update.message.text
+    user = update.message.from_user
+    logger.info("name of %s: %s", user.first_name, name)
+    validation = "Aya in esm va familit hast?   "+str(name)
+    reply_keyboard = [['talash dobare?', 'hamine agha, berim']]
+    update.message.reply_text(validation, reply_markup = ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder='name validation?'
+        ))
+    return NAME_VALIDATION
+
+
+def nameValidation(update, context):
+    validation = update.message.text
+    if validation=='talash dobare?':
+        getName = "pas Lotfan esm va familit ro vared kon"
+        update.message.reply_text(getName)
+        return GET_NAME
+    elif validation == 'hamine agha, berim':
+        return 
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -31,12 +75,21 @@ def main():
 
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            GET_ID: [MessageHandler(Filters.regex('^40031.{3}'), setID)],
+            GET_NAME: [MessageHandler(Filters.text & ~Filters.command, nameSet)],
+            NAME_VALIDATION: [
+                MessageHandler(Filters.location, location),
+                CommandHandler('skip', skip_location),
+            ],
+            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
 
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # for loging all errors when ocurred 
-    dp.add_error_handler(error)
+    dispatcher.add_handler(conv_handler)
 
     # to start the Bot
     updater.start_polling()
