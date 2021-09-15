70c1a34f40e8c299f30f61fb13f0fd0032f316a9
neginkheirmand <neginkheirmand@gmail.com>
2021-09-15 17:47:41 +0430

    update bot, now has parenthood service

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-GET_ID, GET_NAME, NAME_VALIDATION = range(3)
+GET_ID, GET_NAME, NAME_VALIDATION, SERVICE_LIST, GET_SERVICE, WAIT_PARENTHOOD = range(5)
 
 def start(update, context):
     global start_message
@@ -42,6 +42,10 @@ def setID(update, context):
     update.message.reply_text(getName)
     return GET_NAME
     
+def incorrectID(update, context):
+    update.message.reply_text("baba dorost hesabi shomare daneshjuEto bezan dige, laelahaelalah")
+    return GET_ID
+
 def nameSet(update, context):
     name = update.message.text
     user = update.message.from_user
@@ -58,15 +62,73 @@ def nameValidation(update, context):
     validation = update.message.text
     if validation=='talash dobare?':
         getName = "pas Lotfan esm va familit ro vared kon"
-        update.message.reply_text(getName)
+        update.message.reply_text(getName, reply_markup=ReplyKeyboardRemove())
         return GET_NAME
     elif validation == 'hamine agha, berim':
-        return 
+        return SERVICE_LIST
+    else:
+        update.message.reply_text("emmm, yeki az gozine haro lotfan entekhab kon")
+        return NAME_VALIDATION
+
+def listServices(update, context):
+    serviceIntro = """ service hayi ke mn be to {} aziz erae midam inas. Age barat jaleb bud, yekishun ro entekhab kon ta darbarash behet begam :)""".format("ESM_TO") 
+    services = [['Yatim paziri']]
+    update.message.reply_text(serviceIntro, reply_markup = ReplyKeyboardMarkup(
+            services, one_time_keyboard=True, input_field_placeholder='list of service'
+        ))
+    return GET_SERVICE
+
+def setService(update, context):
+    service = update.message.text
+    if service == 'Yatim paziri':
+        global parenthood_message
+        update.message.reply_text(parenthood_message, reply_markup=ReplyKeyboardRemove())
+        options = [['❇sarparast mikham❇', 'Nah!']]
+        update.message.reply_text("khob hala begu bebinam mikhai sarparast dashte bashi? ", ReplyKeyboardMarkup(
+            options, one_time_keyboard=True, input_field_placeholder='list of service'
+        ))
+        return WAIT_PARENTHOOD
+    else: 
+        services = [['Yatim paziri']]
+        update.message.reply_text("Nadashtim hamchin serviceE ha🤔🤔, yebar dige entekhab kon", reply_markup = ReplyKeyboardMarkup(
+            services, one_time_keyboard=True, input_field_placeholder='list of service'
+        ))
+        return GET_SERVICE
+
+def getParenthoodService(update, context):
+    parenthood = update.message.text
+    if parenthood=="❇sarparast mikham❇":
+        update.message.reply_text("hale, be {} khabar midam ke biad be sarparasti ghabulet kone".format("STUDENT_ID_PARENT"), reply_markup=ReplyKeyboardRemove())
+        return SERVICE_LIST
+    elif parenthood == "Nah!":
+        update.message.reply_text("eh chera? \nKhob ok harjur rahat tari, be {} khabar midam ke donbal ye bache dige bashe".format("STUDENT_ID_PARENT"), reply_markup=ReplyKeyboardRemove())
+        return SERVICE_LIST
+    else:
+        update.message.reply_text("agha mn ke nafahmidam chi migi, az gozine ha yekio entekhab kon ya /skip bezan")
+        return WAIT_PARENTHOOD
+
+    
+
+def noSkipGetId(update, context):
+    update.message.reply_text("laelahaelalah, sare karie? ")
+    return GET_ID
+
+def noSkipGetName(update, context):
+    update.message.reply_text("mage mishe mn esmet ro nadunam? noch noch, nemishe")
+    return GET_NAME
+
+def skipParenthood(update, context):
+    update.message.reply_text("inghadr madar pedaret ro hers nade", reply_markup=ReplyKeyboardRemove())
+    return SERVICE_LIST
 
 
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def cancel(update, context):
+    update.message.reply_text("laelahaelalah, berim az aval pas", reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text("dobare shomare daneshjuEt ro vared kon.")
+    return GET_ID
 
 
 def main():
@@ -78,18 +140,20 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            GET_ID: [MessageHandler(Filters.regex('^40031.{3}'), setID)],
-            GET_NAME: [MessageHandler(Filters.text & ~Filters.command, nameSet)],
+            GET_ID: [MessageHandler(Filters.regex('^40031.{3}'), setID), CommandHandler('skip', noSkipGetId), MessageHandler(Filters.text & ~Filters.command), incorrectID],
+            GET_NAME: [MessageHandler(Filters.text & ~Filters.command, nameSet), CommandHandler('skip', noSkipGetName)],
             NAME_VALIDATION: [
-                MessageHandler(Filters.location, location),
-                CommandHandler('skip', skip_location),
+                MessageHandler(Filters.text & ~Filters.command, nameValidation )
+                # SERVICE_LIST, GET_SERVICE, WAIT_PARENTHOOD
             ],
-            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+            SERVICE_LIST:[MessageHandler(Filters.text & ~Filters.command, listServices)],
+            GET_SERVICE:[MessageHandler(Filters.text & ~Filters.command, setService)],
+            WAIT_PARENTHOOD:[ MessageHandler( Filters.text & ~Filters.command, getParenthoodService) , CommandHandler('skip', skipParenthood)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
 
-    dispatcher.add_handler(conv_handler)
+    dp.add_handler(conv_handler)
 
     # to start the Bot
     updater.start_polling()
