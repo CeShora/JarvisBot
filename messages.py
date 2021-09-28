7083c14146758752e0ca0cef7c6af288afbae1cb
neginkheirmand <neginkheirmand@gmail.com>
2021-09-28 14:53:58 +0330

    the cringy farC text was added

--- messages.py
+++ messages.py
@@ -1,10 +1,15 @@
-start_message = """Salam👋 khosh umadi🎉🥳🎉🥳🎉🥳🎉
+start_message_LAELAHAELALAH = """Salam👋 khosh umadi🎉🥳🎉🥳🎉🥳🎉
 man jarvis😉 hastam, dastyar to tuye hafte haye avalet tuye 
 daneshgah va daneshkade, gharare behet komak konam yekam 
 rahat tar va sari tar ja biofT, soalat ro javab bedam va 
 moshkelatet ro hal konam."""
 
-parenthood_message = """khob, service pedar👨‍👦 madar👩‍👦 yabi. Inja gharare ye pedar/madar 
+start_message_FA = """سلام 👋
+خوش اومدی🥳🥳
+من جارویس 😁 هستم؛ دستیارت تو هفته‌‌های اول دانشگاه و دانشکده.
+من اینجام تا به سوالات جواب بدم"""
+
+parenthood_message_LAELAHAELALAH = """khob, service pedar👨‍👦 madar👩‍👦 yabi. Inja gharare ye pedar/madar 
 peida konin🎉. Vali darim ziadi jelo mirim, ehtemalan alan 
 aslan nemidunin pedar/madar daneshjuE chi hast.
 Age be shomareye daneshjuEtun deghat karde bashin ye 
@@ -22,7 +27,7 @@ ke in rasm faghat tuye daneshgahe AUT hast, ya laaghal injas
 ke vaghean jedi gerefte mishe. Pedar/Madar khodetun ro age 
 mikhain beshnaCn mn behetun komak khahad kard. """
 
-tashakolat_message = """AUT ye serie tashakol👥 dare ke darvaghe ye guruh az bachehaye 
+tashakolat_message_LAELAHAELALAH = """AUT ye serie tashakol👥 dare ke darvaghe ye guruh az bachehaye 
 khode daneshgah hastan ke (ba tavajoh be tashakoleshun) ye tedadi 
 az service haro erae midan be bachehaye dige.
 1)✨Shoraye senfi✨: Ehtemalan in tashakol ziba :)) ro ghabl az baghie 
@@ -54,7 +59,22 @@ va 💬telegram💬:
 -inja hamina ro migim bara fanus(?) -
 ..."""
 
-sites_message= """ 
+tashakolat_message_FA = """اینجا در AUT یه سری تَشَکُل هستش که در واقعا گروه‌های مختلف از بچه‌های خود دانشگاه هستن که یه سری از کارها رو انجام میدن؛ مثلا:
+
+1️⃣ شورای صنفی:
+احتمالا این اسم رو بیشتر از بقیه شنیده باشید😁 اینجا کارهای مختلفی مثل آموزش (موقع انتخاب واحد بیشتر آشنا میشید😁)، فرهنگی(پویش، کلاب‌ فیلم و سریال، کوهنوردی و ...)، روابط عمومی(از اسمش مشخصه دیگه)، صنفی(کارای فنی دانشکده)، ورزشی(اینم واضحه😁) و در نهایت بخش مالی انجام میشه و از حقوق بچه‌ها دفاع میکنه.
+در واقع شورا مثل وکیل شما تو دانشگاس.
+اینم از اعضای شورای صنفی:
+...
+...
+...
+...
+...
+...
+...
+اینجا باید اضافه بشه***"""
+
+sites_message_LAELAHAELALAH = """ 
 🌐site haye daneshgah🌐:
 1️⃣ ye courses darim ke tuye doran majazi platform asli baraye 
 upload tamarin o emtehan o ... bude:
@@ -69,3 +89,12 @@ va dar akhar
 4️⃣ ye samane samad ham darim ke felan be dardi nemikhore, 
 ghablan tush bacheha ghazaye self ro reserve mikardan:
 https://samad.aut.ac.ir/"""
+
+sites_message_FA = """سایت‌های aut:
+1️⃣ یه کورسز داریم که تو دوران مجازی، سایت اصلی برای آپلود تمرینات، برگزارری امتحانات و ... است
+
+2️⃣ یه lms داریم که سایت اصلی برای آموزشه
+
+3️⃣ یه پورتال آموزشی هست که باهاش هر ترم انتخاب واحد میکنی و نمرات درخشانت رو میتونی اونجا ببینی. و یه سری کارای دیگه مثل ارزیابی اساتید هم اونجا انجام میشه
+
+4️⃣ یه سامانه سماد هم هست که فعلا کاربردی نداره، قبلا بچه‌ها اونجا غذای سلف رو رزور میکردن"""
