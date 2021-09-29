1c0c2070cd450280fd2419de4335f51c286adc53
neginkheirmand <neginkheirmand@gmail.com>
2021-09-30 01:08:33 +0330

    actually saving the data to a csv file

---
+++ SaveRedisData.py
@@ -0,0 +1,52 @@
+import redis
+import csv
+from datetime import date
+import json
+
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+allData = []
+for key in r.keys():
+    # print(key, r.get(key))
+    data = json.loads(r.get(key))
+    allData.append(data)
+
+# print(allData)
+# print()
+today = date.today()
+# dd/mm/YY
+d1 = today.strftime("%d-%m-%Y")
+
+with open('data'+str(d1)+".csv", mode='w',  encoding='utf16') as dataFile:
+    dataFile = csv.writer(dataFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    dataFile.writerow(['userId', 'chatId', 'username', 'studentID' , 'fullName', 'WantsChild' , 'parentHood' , 'lang'])
+
+    for i in range(0, len(allData)):
+
+        dictionary = allData[i]
+        a = dictionary['userId']
+        b = dictionary['chatId']
+        c = dictionary['username']
+        d = '-none-'
+        e = '-none-'
+        f = '-none-'
+        g = '-none-'
+        h = '-none-'
+
+        if 'studentID' in dictionary:
+            d = dictionary['studentID']
+
+        if 'fullName' in dictionary:
+            e = dictionary['fullName']
+            
+        if 'WantsChild' in dictionary:
+            f = dictionary['WantsChild']
+        
+        if 'parentHood' in dictionary:
+            g = dictionary['parentHood']
+        
+        if 'lang' in dictionary:
+            h = dictionary['lang']
+        print(a, b , c ,d , e , f, g, h)
+        dataFile.writerow( [a, b , c ,d , e , f, g, h] )
