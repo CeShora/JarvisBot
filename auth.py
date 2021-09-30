f8cf57a52aa2a6153cc1a2526ba4aada5879cb6a
neginkheirmand <neginkheirmand@gmail.com>
2021-09-30 17:14:18 +0330

    add auth for the bot

--- auth.py
+++ auth.py
@@ -1,2 +1,10 @@
-def isAuth():
-    return True
+import redis
+import json
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+def isAuth(username):
+    colledctedData = r.get('StudentId_Username_CollectedData')
+    colledctedDataDictionary = json.loads(r.get(colledctedData))
+    if not colledctedDataDictionary[username] == None :
+        return True
